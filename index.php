1a8c095d9239d0d38c136b70357bd133a05cec7a
ichinosee <67300586+ichinosee@users.noreply.github.com>
2021-05-12 23:11:48 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Nose</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-left">NoseCompany</div>
+        <div class="header-right">
+            <ul>
+                <li>会社概要</li>
+                <li>採用</li>
+                <li class="selected">お問い合わせ</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="contact-form">
+            <div class="form-title"></div>
+            <form action="sent.php" method="post"></form>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer-left">
+            <ul>
+                <li>会社概要</li>
+                <li>採用</li>
+                <li>お問い合わせ</li>
+            </ul>
+        </div>
+        <div class="like-box">
+        <iframe src="https://github.com/ichinosee" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+        </div>
+
+
+
+
+</body>
+
+
+
+
+</html>
